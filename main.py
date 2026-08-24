e1dd5e0d0344fd3e64e8b4689abaebb1acd82e43
lcolon231 <luis.acolon03@gmail.com>
2026-08-24 19:19:10 -0400

    Users endpoints added and tested

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
 
-from routers import test, transactions, users
 
-app = FastAPI()
-
-app.include_router(test.router)
-app.include_router(transactions.router)
 app.include_router(users.router)
 
-@app.get("/")
-def root():
-    return {"message": "Banking API is running"}
